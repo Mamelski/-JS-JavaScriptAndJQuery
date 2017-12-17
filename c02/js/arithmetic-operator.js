587a01c0a0cf953c20db3a4fb3202debaba30373
Mamelski <jakub.mamelski@outlook.com>
2017-12-17 21:56:55 +0100

    varaibles names improvement

--- c02/js/arithmetic-operator.js
+++ c02/js/arithmetic-operator.js
@@ -8,16 +8,16 @@ var shipping = 0.5 * (13 + 1); // Shipping is 7
 var total = subtotal + shipping; // Total is 77
 
 // Write the results to the screen
-var elSub = document.getElementById('subtotal');
+var elSubtotal = document.getElementById('subtotal');
 elSub.textContent = subtotal;
 
-var elShip = document.getElementById('shipping');
+var elShipping = document.getElementById('shipping');
 elShip.textContent = shipping;
 
 var elTotal = document.getElementById('total');
 elTotal.textContent = total;
 
-/* 
+/*
 NOTE: textContent does not work in IE8 or earlier
 You can use innerHTML on lines 12, 15, and 18 but note the security issues on p228-231
 elSub.innerHTML = subtotal;
