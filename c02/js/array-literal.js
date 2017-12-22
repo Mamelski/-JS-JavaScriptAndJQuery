3de70890ad181849377ad020d90fc96d817ac692
Mamelski <jakub.mamelski@outlook.com>
2017-12-22 21:02:34 +0100

    arrays

--- c02/js/array-literal.js
+++ c02/js/array-literal.js
@@ -1,5 +1,5 @@
 // Create an array and assign it values.
-var colors; colors = ['white', 'black', 'custom'];
+var colors = ['white', 'black', 'custom'];
 // Show the first item from the array.
 var el = document.getElementById('colors');
 el.textContent = colors[0];
