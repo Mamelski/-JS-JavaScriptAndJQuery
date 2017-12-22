f5309674fe36ef235494889505b5b4f96878c076
Mamelski <jakub.mamelski@outlook.com>
2017-12-22 20:51:52 +0100

    update c=variable

--- c02/js/update-variable.js
+++ c02/js/update-variable.js
@@ -1,1 +1,15 @@
-// Create variables and assign them valuesvar inStock;var shipping;inStock = true;shipping = false;/* Some other processing might go here and, as a result, the script might need to change these values. */inStock = false;shipping = true;// Get the element with an id of stockvar elStock = document.getElementById('stock');// Set class name with value of inStock variableelStock.className = inStock;// Get the element with an id of shippingvar elShip = document.getElementById('shipping');// Set class name with value of shipping variableelShip.className = shipping;
+// Create variables and assign them values
+var inStock = true;
+var shipping= false;
+/* Some other processing might go here and, as a result, the script might need to change these values. */
+
+inStock = false;
+shipping = true;
+// Get the element with an id of stock
+var elStock = document.getElementById('isInStock');
+// Set class name with value of inStock variable
+elStock.className = inStock;
+// Get the element with an id of shipping
+var elShip = document.getElementById('isShipping');
+// Set class name with value of shipping variable
+elShip.className = shipping;
