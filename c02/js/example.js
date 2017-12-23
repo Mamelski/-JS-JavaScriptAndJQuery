7475147d740843e8333b4e3b7f9812c062688c11
Mamelski <jakub.mamelski@outlook.com>
2017-12-23 19:50:37 +0100

    example

--- c02/js/example.js
+++ c02/js/example.js
@@ -3,19 +3,20 @@ var greeting = 'Howdy ';
 var name = 'Molly';
 var message = ', please check your order:';
 // Concatenate the three variables above to create the welcome message
-var welcome = greeting + name + message;
+var welcomeMessage = greeting + name + message;
 
 // Create variables to hold details about the sign
-var sign = 'Montague House';
+var sign = 'Po co tyle komentarzy ???';
 var tiles = sign.length;
-var subTotal = tiles * 5;
+var priceForOneTile = 6;
+var subTotal = tiles * priceForOneTile;
 var shipping = 7;
 var grandTotal = subTotal + shipping;
 
 // Get the element that has an id of greeting
 var el = document.getElementById('greeting');
 // Replace the content of that element with the personalized welcome message
-el.textContent = welcome;
+el.textContent = welcomeMessage;
 
 // Get the element that has an id of userSign then update its contents
 var elSign = document.getElementById('userSign');
@@ -37,7 +38,7 @@ elShipping.textContent = '$' + shipping;
 var elGrandTotal = document.getElementById('grandTotal');
 elGrandTotal.textContent = '$' + grandTotal;
 
-/* 
+/*
 NOTE: textContent does not work in IE8 or earlier
 You can use innerHTML instead of textContent, but note the security issues on p228-231
 
