9a334f37ccb11c7387d03a9b6780e553fe9c8d2c
Mamelski <jakub.mamelski@outlook.com>
2017-12-24 17:06:13 +0100

    object literal

--- c03/js/object-literal.js
+++ c03/js/object-literal.js
@@ -1,6 +1,6 @@
 // Set up the object
 var hotel = {
-  name : 'Quay',
+  name : 'motel',
   rooms : 40,
   booked : 25,
   checkAvailability : function() {
@@ -12,7 +12,7 @@ var hotel = {
 var elName = document.getElementById('hotelName'); // Get element
 elName.textContent = hotel.name;                   // Update HTML with property of the object
 
-var elRooms = document.getElementById('rooms');    // Get element
+var elRooms = document.getElementById('roomsLeft');    // Get element
 elRooms.textContent = hotel.checkAvailability();   // Update HTML with property of the object
 
 /* 
