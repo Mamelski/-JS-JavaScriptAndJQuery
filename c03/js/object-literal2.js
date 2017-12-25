b06eb90c20fc8b155d25c47b079d92dd631548ab
Mamelski <jakub.mamelski@outlook.com>
2017-12-25 18:37:14 +0100

    object-literal2

--- c03/js/object-literal2.js
+++ c03/js/object-literal2.js
@@ -3,7 +3,7 @@ var hotel = {
   name : 'Park',
   rooms : 120,
   booked : 77,
-  checkAvailability : function() {
+  roomsAvailable : function() {
     return this.rooms - this.booked; // Need "this" because inside function
   }
 };
@@ -13,9 +13,9 @@ var elName = document.getElementById('hotelName'); // Get element
 elName.textContent = hotel.name;                   // Update HTML with property of the object
 
 var elRooms = document.getElementById('rooms');    // Get element
-elRooms.textContent = hotel.checkAvailability();   // Update HTML with property of the object
+elRooms.textContent = hotel.roomsAvailable();   // Update HTML with property of the object
 
-/* 
+/*
 NOTE: textContent does not work in IE8 or earlier
 You can use innerHTML on lines 13 and 16, but note the security issues on p228-231
 */
